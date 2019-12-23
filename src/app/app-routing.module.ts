0a5169a7a9d1ad74eb863fc53a24e30afd7e8323
shanerbo <shanerbo@gmail.com>
2019-12-22 17:31:27 -0800

    add cart page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { NoAccessComponent } from './pages/no-access/no-access.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
+
 
 
 const routes: Routes = [
   { path: '', component: MainPageComponent },
   { path: 'login', component: LoginPageComponent },
+  { path: 'cart', component: ShoppingCartComponent },
   { path: 'no-access', component: NoAccessComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
