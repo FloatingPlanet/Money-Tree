53304cdb68e557da9e703a63261cc9ae51431699
shanerbo <shanerbo@gmail.com>
2019-12-22 22:17:57 -0800

    add product items

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -9,10 +9,10 @@ export class ProductItemComponent implements OnInit {
 
   constructor() { }
   products = [
-    { name: "Product A", titles: { title: "title A", subTitle: "subTitle A", price: "CAD 100" }, picture: { uri: 'https://dummyimage.com/600x150/000/fff', thumbNail: 'https://dummyimage.com/600x150/000/fff' } },
-    { name: "Product B", titles: { title: "title B", subTitle: "subTitle B", price: "CAD 110" }, picture: { uri: 'https://dummyimage.com/300x300/000/fff', thumbNail: 'https://dummyimage.com/600x150/000/fff' } },
-    { name: "Product C", titles: { title: "title C", subTitle: "subTitle C", price: "CAD 120" }, picture: { uri: 'https://dummyimage.com/300x400/000/fff', thumbNail: 'https://dummyimage.com/600x150/000/fff' } },
-    { name: "Product D", titles: { title: "title D", subTitle: "subTitle D", price: "CAD 130" }, picture: { uri: 'https://dummyimage.com/600x500/000/fff', thumbNail: 'https://dummyimage.com/600x150/000/fff' } }]
+    { name: "Product A", titles: { title: "title A", subTitle: "subTitle A" }, price: "CAD 100", picture: { uri: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16touch-space-select-201911?wid=400&hei=400&fmt=jpeg&qlt=95&op_usm=0.5,1.5&fit=constrain&.v=1572825197207', thumbNail: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16touch-space-select-201911?wid=400&hei=400&fmt=jpeg&qlt=95&op_usm=0.5,1.5&fit=constrain&.v=1572825197207' } },
+    { name: "Product B", titles: { title: "title B", subTitle: "subTitle B" }, price: "CAD 110", picture: { uri: 'https://dummyimage.com/300x300/000/fff', thumbNail: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16touch-space-select-201911?wid=400&hei=400&fmt=jpeg&qlt=95&op_usm=0.5,1.5&fit=constrain&.v=1572825197207' } },
+    { name: "Product C", titles: { title: "title C", subTitle: "subTitle C" }, price: "CAD 120", picture: { uri: 'https://dummyimage.com/300x400/000/fff', thumbNail: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16touch-space-select-201911?wid=400&hei=400&fmt=jpeg&qlt=95&op_usm=0.5,1.5&fit=constrain&.v=1572825197207' } },
+    { name: "Product D", titles: { title: "title D", subTitle: "subTitle D" }, price: "CAD 130", picture: { uri: 'https://dummyimage.com/600x500/000/fff', thumbNail: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16touch-space-select-201911?wid=400&hei=400&fmt=jpeg&qlt=95&op_usm=0.5,1.5&fit=constrain&.v=1572825197207' } }]
   ngOnInit() {
   }
 
