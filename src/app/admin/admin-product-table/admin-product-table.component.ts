eb034de1704277eae6e730cbf28f1933267e825c
shanerbo <shanerbo@gmail.com>
2019-12-30 16:18:17 -0800

    add product table

---
+++ src/app/admin/admin-product-table/admin-product-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-product-table',
+  templateUrl: './admin-product-table.component.html',
+  styleUrls: ['./admin-product-table.component.scss']
+})
+export class AdminProductTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/shopping-cart/shopping-cart.component.ts
+++ src/app/pages/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product/product.service';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ps: ProductService) { }
 
   ngOnInit() {
   }
