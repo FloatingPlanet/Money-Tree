0479806270b5fba79a647bcadcc8deb77f75614d
shanerbo <shanerbo@gmail.com>
2019-12-30 23:49:18 -0800

    remove admin-product-table

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+import { SelectionModel } from '@angular/cdk/collections';
+import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product/product.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-admin-products',
@@ -8,9 +12,42 @@ import { ProductService } from 'src/app/services/product/product.service';
 })
 export class AdminProductsComponent implements OnInit {
 
-  constructor(private ps: ProductService) { }
 
+  products: Product[];
+  dataSource = new MatTableDataSource<Product>([]);
+  m = moment;
+  constructor(private ps: ProductService, ) {
+
+  }
   ngOnInit() {
+    this.ps.productsObservalbe.subscribe((res) => {
+      this.products = res;
+      this.dataSource = new MatTableDataSource<Product>(this.products);
+    });
+  }
+  c: string[] = ['select', 'SKU', 'productName', 'productPrice', 'productQuantity', 'productCategory', 'productAddedAt', 'edit'];
+  selection = new SelectionModel<Product>(true, []);
+
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
   }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  masterToggle() {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  checkboxLabel(row?: Product): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.SKU}`;
+  }
 }
