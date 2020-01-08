d8741175c9b7134d190351d71d0fb4aee7227c9d
shanerbo <shanerbo@gmail.com>
2020-01-07 20:27:51 -0800

    add ps

--- src/app/components/order-summary/order-summary.component.ts
+++ src/app/components/order-summary/order-summary.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product/product.service';
 
 @Component({
   selector: 'app-order-summary',
@@ -15,15 +17,10 @@ export class OrderSummaryComponent implements OnInit {
   public estimatedTax: number = 0;
   public recyclingFee: number = 0;
   public total: number;
-  order = [
-    { name: "Product A", titles: { title: "title A", subTitle: "subTitle A" }, price: 200, picture: { uri: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16touch-space-select-201911?wid=400&hei=400&fmt=jpeg&qlt=95&op_usm=0.5,1.5&fit=constrain&.v=1572825197207', thumbNail: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16touch-space-select-201911?wid=400&hei=400&fmt=jpeg&qlt=95&op_usm=0.5,1.5&fit=constrain&.v=1572825197207' } },
-    { name: "Product B", titles: { title: "title B", subTitle: "subTitle B" }, price: 110, picture: { uri: 'https://dummyimage.com/300x300/000/fff', thumbNail: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16touch-space-select-201911?wid=400&hei=400&fmt=jpeg&qlt=95&op_usm=0.5,1.5&fit=constrain&.v=1572825197207' } },
-    { name: "Product C", titles: { title: "title C", subTitle: "subTitle C" }, price: 120, picture: { uri: 'https://dummyimage.com/300x400/000/fff', thumbNail: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16touch-space-select-201911?wid=400&hei=400&fmt=jpeg&qlt=95&op_usm=0.5,1.5&fit=constrain&.v=1572825197207' } },
-    { name: "Product D", titles: { title: "title D", subTitle: "subTitle D" }, price: 130, picture: { uri: 'https://dummyimage.com/600x500/000/fff', thumbNail: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16touch-space-select-201911?wid=400&hei=400&fmt=jpeg&qlt=95&op_usm=0.5,1.5&fit=constrain&.v=1572825197207' } }
+  public orders: Product[];
+  constructor(private ps: ProductService) {
 
-  ];
-
-  constructor() { }
+  }
 
 
   ngOnInit() {
