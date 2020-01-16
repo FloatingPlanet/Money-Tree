6e60274e487ff0ec268c424812033da6c963b8b0
jiaranyu <jiarany712@gmail.com>
2020-01-15 22:04:57 -0800

    add show details to cart items add delete item to cart items

---
+++ src/app/components/product-info/product-info.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {Product} from '../../models/product';
+import {ProductService} from "../../services/product/product.service";
+import {ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-product-info',
+  templateUrl: './product-info.component.html',
+  styleUrls: ['./product-info.component.scss']
+})
+export class ProductInfoComponent implements OnInit {
+  public product: Product;
+  private SKU: any;
+
+  constructor(private ps: ProductService, private route: ActivatedRoute) {
+    this.SKU = this.route.snapshot.paramMap.get('SKU');
+    console.log(this.SKU);
+    this.ps.fetchProduct(this.SKU).then((res) => {
+      this.product = res as Product;
+    });
+  }
+
+  ngOnInit() {
+
+  }
+
+}
