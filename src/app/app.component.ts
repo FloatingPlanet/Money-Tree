ccfe398253418ff6bd8495241c1b7860f8d49cb1
shanerbo <shanerbo@gmail.com>
2020-01-26 00:25:26 -0800

    user log out no longer redirect to root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from './services/login/auth.service';
 import {UserService} from './services/user/user.service';
 
@@ -7,20 +7,26 @@ import {UserService} from './services/user/user.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'money-tree';
   DEFAULT_AVATAR = '../../../assets/default-avatar.png';
   DEFAULT_NAME = 'Shady Individual';
   logInfo: any;
 
   constructor(private as: AuthService) {
+
+  }
+
+  ngOnInit(): void {
     this.as.currentUserObservable.subscribe((auth) => {
       this.logInfo = {
         avatarURL: auth ? auth.photoURL : this.DEFAULT_AVATAR,
         displayName: auth ? auth.displayName : this.DEFAULT_NAME,
         authState: !!auth
       };
+      console.log(this.logInfo)
     });
   }
+
 
 }
