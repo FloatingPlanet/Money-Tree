246d2e2aafdbfede53f41d13367e7480ba021fa3
shanerbo <shanerbo@gmail.com>
2020-02-05 23:28:55 -0800

    show details using <app-product-item>

--- src/app/components/product-quick-look/product-quick-look.component.ts
+++ src/app/components/product-quick-look/product-quick-look.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {GermanAddress} from '@angular-material-extensions/google-maps-autocomplete';
 
 @Component({
   selector: 'app-product-quick-look',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-quick-look.component.scss']
 })
 export class ProductQuickLookComponent implements OnInit {
+  private Key = 'EE77-JU96-FM31-NM51';
+  private SearchTerm: string;
+  private LocationAccuracy: number;
+  private Country: string;
+  private LanguagePreference: string;
+  private Locations: string;
+  private addr: string;
 
-  constructor() { }
+  constructor() {
+  }
+
 
   ngOnInit() {
+
   }
+
 
 }
