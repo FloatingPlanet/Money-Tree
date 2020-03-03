5f9c3c594d4476db16996589c92dd4457e454c4b
shanerbo <shanerbo@gmail.com>
2020-03-02 21:05:02 -0800

    update project to angular 9.x

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,7 +40,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
-// Our stuff ---- ❤❤❤
+// Our stuff
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
