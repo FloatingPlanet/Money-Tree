fa9d5d6c11d073e1d7cec111c075012163f3bdaa
shanerbo <shanerbo@gmail.com>
2020-03-06 23:32:48 -0800

    Revert "Delete environment.ts"
    This reverts commit e3e80691

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCoU3fwV8n-s7Exwc5nWhVllCzK-957Kjs',
+    authDomain: 'fake-money-tree.firebaseapp.com',
+    databaseURL: 'https://fake-money-tree.firebaseio.com',
+    projectId: 'fake-money-tree',
+    storageBucket: 'fake-money-tree.appspot.com',
+    messagingSenderId: '236698827163',
+    appId: '1:236698827163:web:385ef6dec721558d1b493e'
+  }
+}
+;
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
