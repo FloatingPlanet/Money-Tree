52cc268a38a8f82187d7cab50efd77332a0448d2
shanerbo <shanerbo@gmail.com>
2020-03-14 13:53:06 -0700

    change gradient color parttern, add mdboostrap dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -83,10 +83,13 @@ import {AddressService} from './services/address/address.service';
 import { MidBannerComponent } from './components/mid-banner/mid-banner.component';
 import { CategoriesBarComponent } from './components/categories-bar/categories-bar.component';
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { CategoryProductsPageComponent } from './pages/category-products-page/category-products-page.component';
 import { CategoryProductsGridComponent } from './pages/category-products-page/category-products-grid/category-products-grid.component';
 import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import { faFacebookF, faTwitch, faGooglePlus, faLinkedinIn, faDribbble } from '@fortawesome/free-brands-svg-icons';
+
 
 @NgModule({
   declarations: [
@@ -122,6 +125,7 @@ import {faSearch} from '@fortawesome/free-solid-svg-icons';
     LoadingSpinnerComponent,
     CategoryProductsPageComponent,
     CategoryProductsGridComponent,
+    FooterComponent,
   ],
   imports: [
     AngularFireModule.initializeApp(environment.firebaseConfig),
@@ -188,7 +192,7 @@ import {faSearch} from '@fortawesome/free-solid-svg-icons';
 
 export class AppModule {
   constructor(private library: FaIconLibrary) {
-    library.addIcons(faSearch);
+    library.addIcons(faSearch, faFacebookF, faTwitch, faGooglePlus, faLinkedinIn, faDribbble);
   }
 }
 
