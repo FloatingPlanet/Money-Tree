8d075108ba66e565c2b5a4478fa83d63957fc427
shanerbo <shanerbo@gmail.com>
2020-03-15 15:48:52 -0700

    add collapse button in admin page

--- src/app/pages/admin-page/admin-page.component.ts
+++ src/app/pages/admin-page/admin-page.component.ts
@@ -6,6 +6,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./admin-page.component.scss']
 })
 export class AdminPageComponent implements OnInit {
+  showFiller = true;
 
   constructor() {
 
