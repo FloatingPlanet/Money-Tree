9280c3f6a995b45b257d64dffcfb4a8254a221c5
jiaranyu <jiarany712@gmail.com>
2020-03-16 17:04:04 -0700

    nav-bar-with-bug

--- src/app/components/categories-bar/categories-bar.component.ts
+++ src/app/components/categories-bar/categories-bar.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {CategoryService} from '../../services/category/category.service';
 import {Category} from '../../models/category';
 import {Product} from '../../models/product';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-categories-bar',
@@ -11,8 +12,16 @@ import {Product} from '../../models/product';
 export class CategoriesBarComponent implements OnInit {
   public allCategories: Category[];
   public cat: Category;
+  public currentCat: string;
 
-  constructor(private cs: CategoryService) {
+  constructor(private cs: CategoryService,  public route: ActivatedRoute) {
+    this.route.params.subscribe((params) => {
+      // keep eye on route changes, if so re-render products
+      const cat = params.section;
+      console.log('this is ' + cat);
+      console.log('this is ' + this.currentCat);
+    });
+
     this.cs.categoriesObservable.subscribe((cats) => {
       this.allCategories = cats;
     });
@@ -22,7 +31,7 @@ export class CategoriesBarComponent implements OnInit {
   }
 
   public getProducts(category: string) {
-    this.cs.specificCategoryProductsObservable(category.toUpperCase()).subscribe((res: Product[])=>{
+    this.cs.specificCategoryProductsObservable(category.toUpperCase()).subscribe((res: Product[]) => {
       console.log(res);
     });
   }
